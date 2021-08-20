156e8c68dc21c78f354534f2c20ec90c32f40f07
Xurvan <xurvan@tutanota.com>
2021-08-20 15:36:24 +0430

    Replace HTTP status code with DRF statics

--- purchase/exceptions.py
+++ purchase/exceptions.py
@@ -1,12 +1,13 @@
-from rest_framework.exceptions import APIException
 from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException
 
 
 class LimitExceededError(APIException):
-    status_code = 403
+    status_code = status.HTTP_403_FORBIDDEN
     default_detail = _('order limit exceeded')
 
 
 class EmptyPriceError(APIException):
-    status_code = 422
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
     default_detail = _('price not found')
