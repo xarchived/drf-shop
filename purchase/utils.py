8b7ce8e0916cc9d353c66db5d2e23c9627780991
Xurvan <xurvan@tutanota.com>
2021-09-09 10:09:03 +0430

    Check packages in subscribe query

--- purchase/utils.py
+++ purchase/utils.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-
 from django.db.models import Q, F, QuerySet
 from django.utils import timezone
 
@@ -10,11 +9,21 @@ from purchase.models import Subscribe, Product, Order, Price
 
 def active_subscribes(user_id: int) -> QuerySet:
     return Subscribe.objects.filter(
-        Q(orders__inserted_at__gt=timezone.now() - timedelta(days=1) * F('duration')) &
-        Q(orders__payments__ref_id__isnull=False) &
         (
-                Q(orders__user_id=user_id) |
-                Q(orders__user__children__id=user_id)
+                Q(orders__inserted_at__gt=timezone.now() - timedelta(days=1) * F('duration')) &
+                Q(orders__payments__ref_id__isnull=False) &
+                (
+                        Q(orders__user_id=user_id) |
+                        Q(orders__user__children__id=user_id)
+                )
+        ) |
+        (
+                Q(packages__orders__inserted_at__gt=timezone.now() - timedelta(days=1) * F('duration')) &
+                Q(packages__orders__payments__ref_id__isnull=False) &
+                (
+                        Q(packages__orders__user_id=user_id) |
+                        Q(packages__orders__user__children__id=user_id)
+                )
         )
     ).distinct()
 
