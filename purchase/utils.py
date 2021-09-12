dbba39e9f4b5cf298121e7a78428b0946048185d
Xurvan <xurvan@tutanota.com>
2021-09-12 15:31:15 +0430

    Fix active products query for packages

--- purchase/utils.py
+++ purchase/utils.py
@@ -31,7 +31,7 @@ def active_subscribes(user_id: int) -> QuerySet:
 def active_products(user_id: int) -> QuerySet:
     return Product.objects.filter(
         Q(orders__products__in=active_subscribes(user_id=user_id)) |
-        Q(packages__orders__products__in=active_subscribes(user_id=user_id))
+        Q(packages__products__in=active_subscribes(user_id=user_id))
     ).distinct()
 
 
